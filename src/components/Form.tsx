767bd557527fae40f2db391647f28d9b779c0983
Andrijan Tasevski <andrijantasevski@gmail.com>
2022-11-10 03:51:40 +0100

    Added React Hook Form and started collecting values.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,72 +1,119 @@
-import { RadioGroup } from '@headlessui/react'
-import { useEffect, useState } from 'react';
+import { RadioGroup } from "@headlessui/react";
+import { useForm, Controller } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+const tipValues = [5, 10, 15, 25, 50];
 
 const Form: React.FC = () => {
-    const [valueSelect, setValueSelect] = useState(5);
+  const { control, setValue, watch, register, getValues } = useForm();
+  const formChange = watch();
 
-    useEffect(() => {
-        console.log(valueSelect);
-    })
+  useEffect(() => {
+    const { numberOfPeople, tipRadioGroup, billAmount } = getValues();
 
-    return (
-        <form className="flex flex-col gap-3 w-2/4">
-            <div>
-                <label className="flex flex-col gap-2 text-lg text-neutral-900" htmlFor="bill-amount">
-                    <p className="inline-block">Bill</p>
+    console.log(
+      "TIP/PERSON:",
+      ((Number(tipRadioGroup) / 100) * Number(billAmount)) /
+        Number(numberOfPeople)
+    );
 
-                    <div className="inline-flex items-center border-2 border-neutral-900 pl-2">
-                        <p className="w-full">$</p>
+    console.log(numberOfPeople);
+  }, [formChange]);
 
-                        <input type="text" className="text-right border-none bg-transparent text-lg w-full focus:ring-0" placeholder="0" id="bill-amount" />
-                    </div>
-                </label>
+  return (
+    <form className="flex w-2/4 flex-col gap-3">
+      <div>
+        <label
+          className="flex flex-col gap-2 text-lg text-neutral-900"
+          htmlFor="bill-amount"
+        >
+          <p className="inline-block">Bill</p>
+
+          <div className="inline-flex items-center border-2 border-neutral-900 pl-2">
+            <p className="w-full">$</p>
+
+            <input
+              {...register("billAmount")}
+              type="text"
+              className="w-full border-none bg-transparent text-right text-lg focus:ring-0"
+              placeholder="0"
+              id="bill-amount"
+            />
+          </div>
+        </label>
+      </div>
+      <p className="text-lg">Select tip</p>
+      <Controller
+        control={control}
+        defaultValue={5}
+        name="tipRadioGroup"
+        render={({ field: { onChange, value } }) => (
+          <RadioGroup
+            className="grid grid-cols-1 gap-2 text-lg"
+            value={value}
+            onChange={onChange}
+          >
+            <div className="grid grid-cols-3 gap-2">
+              {tipValues.map((tipValue) => {
+                return (
+                  <RadioGroup.Option
+                    key={tipValue}
+                    value={tipValue}
+                    className={({ checked }) =>
+                      `inline-block cursor-pointer border-2 border-neutral-900 p-2 text-center transition-colors ${
+                        checked ? "bg-neutral-900 text-neutral-100" : ""
+                      }`
+                    }
+                  >
+                    <p>{tipValue}%</p>
+                  </RadioGroup.Option>
+                );
+              })}
+
+              <input
+                onClick={() => setValue("tipRadioGroup", 0)}
+                type="text"
+                {...register("customTip")}
+                placeholder="Custom"
+                className={`border-2 border-neutral-900 p-2 text-center text-lg focus:border-neutral-900 focus:ring-0`}
+              />
             </div>
+          </RadioGroup>
+        )}
+      />
+      <div>
+        <label
+          className="flex flex-col gap-2 text-lg text-neutral-900"
+          htmlFor="number-of-people"
+        >
+          <p className="inline-block">Number of people</p>
 
-            <RadioGroup className="text-lg grid grid-cols-1 gap-2" value={valueSelect} onChange={setValueSelect}>
-                <p className='text-lg'>Select tip</p>
-                <div className='grid grid-cols-3 gap-2'>
-                    <RadioGroup.Option value={5} className={({ checked }) => `text-center cursor-pointer p-2 border-2 border-neutral-900 inline-block transition-colors ${checked ? "bg-neutral-900 text-neutral-100" : ""}`}>
-                        <p>5%</p>
-                    </RadioGroup.Option>
+          <div className="inline-flex items-center border-2 border-neutral-900 pl-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 14 15"
+            >
+              <path
+                fill="#000"
+                d="M7 7.776a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm2.333-3.5a2.333 2.333 0 1 1-4.666 0 2.333 2.333 0 0 1 4.666 0ZM14 13.61c0 1.166-1.167 1.166-1.167 1.166H1.167S0 14.776 0 13.61c0-1.167 1.167-4.667 7-4.667s7 3.5 7 4.667Zm-1.167-.005c0-.287-.18-1.15-.97-1.941C11.102 10.903 9.67 10.11 7 10.11c-2.672 0-4.102.793-4.863 1.554-.79.79-.968 1.654-.97 1.94h11.666Z"
+              />
+            </svg>
 
-                    <RadioGroup.Option value={10} className={({ checked }) => `text-center cursor-pointer p-2 border-2 border-neutral-900 inline-block transition-colors ${checked ? "bg-neutral-900 text-neutral-100" : ""}`}>
-                        <p>10%</p>
-                    </RadioGroup.Option>
-
-                    <RadioGroup.Option value={15} className={({ checked }) => `text-center cursor-pointer p-2 border-2 border-neutral-900 inline-block transition-colors ${checked ? "bg-neutral-900 text-neutral-100" : ""}`}>
-                        <p>15%</p>
-                    </RadioGroup.Option>
-                </div>
-
-                <div className='grid grid-cols-3 gap-2'>
-                    <RadioGroup.Option value={25} className={({ checked }) => `text-center cursor-pointer p-2 border-2 border-neutral-900 inline-block transition-colors ${checked ? "bg-neutral-900 text-neutral-100" : ""}`}>
-                        <p>25%</p>
-                    </RadioGroup.Option>
-
-                    <RadioGroup.Option value={50} className={({ checked }) => `text-center cursor-pointer p-2 border-2 border-neutral-900 inline-block transition-colors ${checked ? "bg-neutral-900 text-neutral-100" : ""}`}>
-                        <p>50%</p>
-                    </RadioGroup.Option>
-
-                    <input onClick={() => setValueSelect(0)} type="text" placeholder="Custom" className={`text-center p-2 border-2 border-neutral-900 text-lg focus:ring-0 focus:border-neutral-900`} />
-                </div>
-            </RadioGroup>
-
-            <div>
-                <label className="flex flex-col gap-2 text-lg text-neutral-900" htmlFor="number-of-people">
-                    <p className="inline-block">Number of people</p>
-
-                    <div className="inline-flex items-center border-2 border-neutral-900 pl-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-5' fill="none" viewBox="0 0 14 15">
-                            <path fill="#000" d="M7 7.776a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm2.333-3.5a2.333 2.333 0 1 1-4.666 0 2.333 2.333 0 0 1 4.666 0ZM14 13.61c0 1.166-1.167 1.166-1.167 1.166H1.167S0 14.776 0 13.61c0-1.167 1.167-4.667 7-4.667s7 3.5 7 4.667Zm-1.167-.005c0-.287-.18-1.15-.97-1.941C11.102 10.903 9.67 10.11 7 10.11c-2.672 0-4.102.793-4.863 1.554-.79.79-.968 1.654-.97 1.94h11.666Z" />
-                        </svg>
-
-
-                        <input type="text" className="text-right border-none bg-transparent text-lg w-full focus:ring-0" placeholder="0" id="number-of-people" />
-                    </div>
-                </label>
-            </div>
-        </form >
-    )
-}
+            <input
+              {...register("numberOfPeople", { value: 1 })}
+              type="text"
+              className="w-full border-none bg-transparent text-right text-lg focus:ring-0"
+              placeholder="0"
+              id="number-of-people"
+            />
+          </div>
+        </label>
+      </div>
+      <button>submit</button>
+    </form>
+  );
+};
 
 export default Form;
